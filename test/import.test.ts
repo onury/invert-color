6e710cdcf4fb220eac799d6b6b05e72e0d7b2dae
Onur Yıldırım <onur@cutepilot.com>
2018-11-09 19:49:16 +0300

    update tests

--- test/import.test.ts
+++ test/import.test.ts
@@ -1,8 +1,8 @@
-import invert from '../lib/invert';
+import invert from '..';
 
 describe('test: import / require', () => {
 
-    test('import (default)', () => {
+    test('import invert', () => {
         let err = null;
         try {
             expect(invert('#000')).toEqual('#ffffff');
@@ -12,10 +12,32 @@ describe('test: import / require', () => {
         expect(err).toEqual(null);
     });
 
-    test('require', () => {
+    test('require(\'../lib/invert\')', () => {
         let err = null;
         try {
             const inv = require('../lib/invert');
+            expect(inv(('#000'))).toEqual('#ffffff');
+        } catch (e) {
+            err = e;
+        }
+        expect(err).toEqual(null);
+    });
+
+    test('require(\'../lib/invert.min\')', () => {
+        let err = null;
+        try {
+            const inv = require('../lib/invert.min');
+            expect(inv(('#000'))).toEqual('#ffffff');
+        } catch (e) {
+            err = e;
+        }
+        expect(err).toEqual(null);
+    });
+
+    test('require(\'../lib/cjs/invert\').default', () => {
+        let err = null;
+        try {
+            const inv = require('../lib/cjs/invert').default;
             expect(inv(('#000'))).toEqual('#ffffff');
         } catch (e) {
             err = e;
